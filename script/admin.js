88908190fcc9d281fe6fa36c136c21db0f5acaf1
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-06-19 21:32:51 +0200

    edit

--- script/admin.js
+++ script/admin.js
@@ -9,7 +9,7 @@ console.table(products)
 
 products.forEach(product => {
     let hold = product.id
-    console.log(`${hold}`)
+
     itemsDisplay.innerHTML +=
         `
                 <tr>
@@ -18,7 +18,42 @@ products.forEach(product => {
                   <td>${product.category}</td> 
                   <td>R${product.price}</td>
                   <td><img src="${product.img_url}" alt="${product.productName}">
-                  <td><button type="button" onclick="editItem(this)" value='${hold}' class="bi-pencil-fill"></button>
+                  <td>
+                    
+                    
+                    <button type="button" class=" bi-pencil-fill" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"  onclick="editItemLoad(this)" value='${hold}'></button>
+
+
+                    <div class="modal fade" id="staticBackdrop1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdrop1Label" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5 text-dark" id="staticBackdrop1Label">Edit...</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                             <div class="modalFields">
+                                <label>Name: <input type="text" name="name" required editName></label>
+                                <label>ID: <input type="number" id="id" required readonly editID></label>
+                                <label>Category: <select category editCategory>
+                                    <option value="clothes">clothes</option>
+                                    <option value="figures">figures</option>
+                                    <option value="art">art</option>
+                                </select></label>
+                                <label>Price: <input type="number" price="price" required editPrice></label>
+                                <label>Img url: <input type="text" img_url required editUrl></label>
+                                <label></label>
+
+                           </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="refreshPage()">Close</button>
+                            <button type="button" class="btn btn-primary" onclick="editItemConfirm()">Confirm</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
                     <button type="button" onclick="removeItem(this)" value='${hold}' class="bi-trash3-fill"></button>
                   </td>
 
@@ -27,34 +62,85 @@ products.forEach(product => {
         `
 });
 
-function editItem(button) {
-    console.log(button.value)
-    /* alert(button.value) */
-}
-
-function removeItem(button) {
-try {
-    console.log(button.value)
-    let itemID = button.value
+function editItemLoad(button) {
+    let editID = button.value
+    console.log(editID, button.value)
     let position
-    itemsDisplay.innerHTML = ""
     products.forEach((target, i) => {
-        if (target.id == itemID) {
+        if (target.id == editID) {
             position = i
-            console.log(position)
+            console.log(products[position].productName)
 
-            products.splice(position, 1)
-            localStorage.setItem('products', JSON.stringify(products))
+            let editName = document.querySelector('[editName]')
+            let editID = document.querySelector('[editID]')
+            let editCategory = document.querySelector('[editCategory]')
+            let editPrice = document.querySelector('[editPrice]')
+            let editUrl = document.querySelector('[editUrl]')
+
+
+
+            editName.value = products[position].productName
+            editID.value = products[position].id
+            editCategory.value = products[position].category
+            editPrice.value = products[position].price
+            editUrl.value = products[position].img_url
 
             return;
         }
 
     })
-
-    refreshPage()
-} catch (e) {
-    alert("An error has occured. Reload the page.")
 }
+
+function editItemConfirm() {
+    let Name = document.querySelector('[editName]')
+    let id = document.querySelector('[editID]')
+    let category = document.querySelector('[editCategory]')
+    let price = document.querySelector('[editPrice]')
+    let quantity = "1"
+    let img_url = document.querySelector('[editUrl]')
+    products.push(new Create(id.value, Name.value, category.value, price.value, quantity.value, img_url.value))
+    localStorage.setItem('products', JSON.stringify(products))
+
+    let itemID = id
+    let position
+    products.forEach((target, i) => {
+        if (target.id == itemID) {
+            position = i
+            console.log(position)
+            products.splice(position, 1)
+            localStorage.setItem('products', JSON.stringify(products))
+            
+            return;
+        }
+
+    })
+
+
+}
+
+function removeItem(button) {
+    try {
+        console.log(button.value)
+        let itemID = button.value
+        let position
+        itemsDisplay.innerHTML = ""
+        products.forEach((target, i) => {
+            if (target.id == itemID) {
+                position = i
+                console.log(position)
+
+                products.splice(position, 1)
+                localStorage.setItem('products', JSON.stringify(products))
+
+                return;
+            }
+
+        })
+
+        refreshPage()
+    } catch (e) {
+        alert("An error has occured. Reload the page.")
+    }
 }
 
 class Create {
@@ -82,27 +168,59 @@ function createItem() {
 }
 
 function refreshPage() {
-try {
-    itemsDisplay.innerHTML = ""
+    try {
+        itemsDisplay.innerHTML = ""
 
-    products.forEach(product => {
-        let hold = product.id
-        console.log(`${hold}`)
-        itemsDisplay.innerHTML +=
-            `
+        products.forEach(product => {
+            let hold = product.id
+            console.log(`${hold}`)
+            itemsDisplay.innerHTML +=
+                `
                     <tr>
                       <td>${product.productName}</td>
                       <td>${product.id}</td>
                       <td>${product.category}</td> 
                       <td>R${product.price}</td>
                       <td><img src="${product.img_url}" alt="${product.productName}">
-                      <td><button type="button" onclick="editItem(this)" value='${hold}' class="bi-pencil-fill"></button>
+                      <td>
+                      <button type="button" class="bi-pencil-fill" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"  onclick="editItemLoad(this)" value='${hold}'></button>
+
+
+                    <div class="modal fade" id="staticBackdrop1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdrop1Label" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5 text-dark" id="staticBackdrop1Label">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                             <div class="modalFields">
+                                <label>Name: <input type="text" name="name" required editName></label>
+                                <label>ID: <input type="number" id="id" required readonly editID></label>
+                                <label>Category: <select category editCategory>
+                                    <option value="clothes">clothes</option>
+                                    <option value="figures">figures</option>
+                                    <option value="art">art</option>
+                                </select></label>
+                                <label>Price: <input type="number" price="price" required editPrice></label>
+                                <label>Img url: <input type="text" img_url required editUrl></label>
+                                <label></label>
+
+                           </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="refreshPage()">Close</button>
+                            <button type="button" class="btn btn-primary" onclick="editItemConfirm()">Confirm</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                           <button type="button" onclick="removeItem(this)" value='${hold}' class="bi-trash3-fill"></button></td>    
                                 
                     </tr>
             `
-    });
-} catch (e) {
-    alert("An error has occurred. Reload the page.")
-}
+        });
+    } catch (e) {
+        alert("An error has occurred. Reload the page.")
+    }
 }
--- script/code.js
+++ script/code.js
@@ -7,58 +7,58 @@ let products = JSON.parse(localStorage.getItem('products')) ?
         JSON.stringify(
 
             [{
-                    id: "235",
-                    productName: "Chad 1:6",
-                    category: "figures",
-                    price: 300,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                },
+                id: "235",
+                productName: "Rukia Figure",
+                category: "figures",
+                price: 300,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Rukia.jpg"
+            },
 
-                {
-                    id: "853",
-                    productName: "Akatsuki cloak",
-                    category: "clothes",
-                    price: 800,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                },
+            {
+                id: "853",
+                productName: "Naruto Hoodie",
+                category: "clothes",
+                price: 800,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Naruto.jpg"
+            },
 
-                {
-                    id: "480",
-                    productName: "300x400 Asuka Portrait",
-                    category: "art",
-                    price: 1200,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                },
+            {
+                id: "480",
+                productName: "Sanji Portrait",
+                category: "art",
+                price: 1200,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Sanji.jpg"
+            },
 
-                {
-                    id: "245",
-                    productName: "300x400 Itachi Portrait",
-                    category: "art",
-                    price: 800,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                },
+            {
+                id: "245",
+                productName: "Zorro Portrait",
+                category: "art",
+                price: 800,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Zorro.jpg"
+            },
 
-                {
-                    id: "194",
-                    productName: "Plain sweater pattern",
-                    category: "clothes",
-                    price: 450,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                },
+            {
+                id: "194",
+                productName: "Sasuke Hoodie",
+                category: "clothes",
+                price: 450,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Sasuke.jpg"
+            },
 
-                {
-                    id: "212",
-                    productName: "Ichigo 1:16",
-                    category: "figures",
-                    price: 120,
-                    quantity: 1,
-                    img_url: "https://ismaaeel-a.github.io/allimages/Images/plc.png"
-                }
+            {
+                id: "212",
+                productName: "Renji Figure",
+                category: "figures",
+                price: 120,
+                quantity: 1,
+                img_url: "https://ismaaeel-a.github.io/allimages/Images/Renji.jpeg"
+            }
             ]
         )
     )
